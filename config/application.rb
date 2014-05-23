b982fc3f1b16e78d30a297d1597042db6d699f4e
Caleb Tutty <caleb@prettymint.co.nz>
2014-05-24 10:52:12 +1200

    Changed default sass syntax to sass from scss

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,13 @@ module AxsAkl
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.generators do |g|
+      g.view_specs false
+      g.helper_specs false
+    end
+
+    config.sass.preferred_syntax = :sass
+
   end
 end
