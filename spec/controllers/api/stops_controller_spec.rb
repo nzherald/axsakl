a34dc0674f95d10a14d92d2d275d9f6d8a421ef4
Caleb Tutty <caleb@prettymint.co.nz>
2014-05-24 22:19:15 +1200

    Fixed controller spec paths

---
+++ spec/controllers/api/stops_controller_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe Api::StopsController do
+
+  it 'has an index'
+
+  it 'has a show'
+
+end
--- spec/controllers/stops_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe StopsController do
-
-end
