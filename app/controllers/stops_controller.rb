7a1142ad5500c8e884ef9d6275d695df871161f4
Caleb Tutty <caleb@prettymint.co.nz>
2014-05-24 12:47:41 +1200

    Adding json controller actions for index and show for the stops resource

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -1,2 +1,13 @@
 class StopsController < ApplicationController
+  respond_to :json
+
+  def index
+    @stops = Stop.all
+    respond_with @stops
+  end
+
+  def show
+    @stop = Stop.find(params[:id])
+    respond_with @stop
+  end
 end
