d3c19caff4cf0dfad101050dee25ddd99d2aa0a5
Caleb Tutty <caleb@prettymint.co.nz>
2014-05-25 10:41:56 +1200

    Added Ajax call

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -16,6 +16,13 @@ var map = L.map('map').setView([-36.853282, 174.765896], 13);
 
 var hello = L.layerGroup([L.tileLayer('http://{s}.tiles.mapbox.com/v3/nzherald.gb3l9agk/{z}/{x}/{y}.png')]).addTo(map);
 
+$.ajax({ url: '/api/stops.json',
+         success: function(result) {
+           $.each(result, function(index, value) {
+             L.marker([value.coordinates.coordinates[0], value.coordinates.coordinates[1]]).addTo(map);
+           });
+         }
+});
 
 
 
@@ -48,3 +55,7 @@ $(window).resize(function () {
 		resizeMap();
 	}, 500, "some unique string");
 });
+
+// $(window).resize(function() {
+// 	$map.height(window.innerHeight - headerHeight)
+// });
