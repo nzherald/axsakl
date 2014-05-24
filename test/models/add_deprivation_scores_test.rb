a88e8c6612a8d89689f97e98e80c3a8eb43f24e8
Caleb Tutty <caleb@prettymint.co.nz>
2014-05-24 22:27:33 +1200

    Removed TestUnit cruft in favour of RSpec

--- test/models/add_deprivation_scores_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class AddDeprivationScoresTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/deprivation_score_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class DeprivationScoreTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
