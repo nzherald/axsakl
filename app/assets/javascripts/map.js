f3655f9dd5653ce2451f8a3f6a028800d4c17d1f
Cameron Wright <cam@cam.geek.nz>
2014-05-25 10:12:14 +1200

    Added base map calls

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,1 +1,16 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+// var lat = 0,
+// 	lon = 0,
+// 	foundLocation = function(position){
+// 		lat = position.coords.latitude,
+// 	    lon = position.coords.longitude;
+// 	},
+// 	noLocation = function(){
+// 		lat = -36.853282;
+// 	    lon = 174.765896;
+// 	}
+// ;
+
+// var currentPosition = navigator.geolocation.getCurrentPosition(foundLocation, noLocation);
+var map = L.map('map').setView([-36.853282, 174.765896], 13);
+
+var hello = L.layerGroup([L.tileLayer('http://{s}.tiles.mapbox.com/v3/nzherald.gb3l9agk/{z}/{x}/{y}.png')]).addTo(map);
