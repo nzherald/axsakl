72de9ff874fa80b3deb1e0e25beebac660fe52d1
Cameron Wright <cam@cam.geek.nz>
2014-05-25 10:31:06 +1200

    Added basic map functions

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -11,6 +11,23 @@
 // ;
 
 // var currentPosition = navigator.geolocation.getCurrentPosition(foundLocation, noLocation);
+
+var layer = new L.StamenTileLayer("watercolor");
+
 var map = L.map('map').setView([-36.853282, 174.765896], 13);
 
-var hello = L.layerGroup([L.tileLayer('http://{s}.tiles.mapbox.com/v3/nzherald.gb3l9agk/{z}/{x}/{y}.png')]).addTo(map);
+// map.addLayer(layer);
+
+// var hello = L.layerGroup([L.tileLayer('http://{s}.tiles.mapbox.com/v3/nzherald.gb3l9agk/{z}/{x}/{y}.png')]).addTo(map);
+
+
+
+
+// resize the map
+
+var $map = $('#map'),
+	headerHeight = $('#header').height();
+$map.height(window.innerHeight - headerHeight);
+// $(window).resize(function() {
+// 	$map.height(window.innerHeight - headerHeight)
+// });
